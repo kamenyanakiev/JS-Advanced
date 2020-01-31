39a696728888e179d528253d2a9e282eff05651c
Kamen Yanakiev <kamenqnakiev@gmail.com>
2020-01-31 11:40:54 +0200

    Message encription app done

---
+++ JS-Bootcamp/Projects/04-Messaging/index.js
@@ -0,0 +1,21 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('#message-show h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+  const input = document.querySelector('#message-input');
+  const encripted = btoa(input.value);
+  const link = document.querySelector('#link-input');
+  link.value = `${window.location}#${encripted}`;
+  link.select();
+});
